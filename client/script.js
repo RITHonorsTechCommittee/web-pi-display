6849663de34edbf57c4c92b595c2f65ea5ccfd3b
jforbes <jeannette.forbes@gmail.com>
2018-02-05 20:42:08 -0500

    Added client html, css, js files

---
+++ client/script.js
@@ -0,0 +1,5 @@
+const init = () => {
+  console.log("hi there");
+}
+
+window.onload = init;
--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { res.sendFile('index.html', {root: './client'});});
 app.get('/style.css', (req, res) => {res.sendFile('style.css', {root:'./client'}); });
+app.get('/script.js', (req, res) => {res.sendFile('script.js', {root:'./client'}); });
 
 const run = () => {
   server.listen(PORT);
