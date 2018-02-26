269017bdc576505270a82d3c3a7e6565872f7573
Randy Carlson <randycarlson486@gmail.com>
2018-02-25 23:48:50 -0500

    rotating display of RSS items

--- client/script.js
+++ client/script.js
@@ -1,7 +1,11 @@
 let timeDiv;
+let items;
+let currentItem = 0;
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let parser = new DOMParser();
 
 const init = () => {
     timeDiv = document.querySelector('#time');
@@ -28,7 +32,7 @@ const getDate = () => {
 const getTime = () => {
     let date = new Date();
     let hours = date.getHours();
-    var period = (hours > 12) ? "PM" : "AM";
+    var period = (hours > 12) ? 'PM' : 'AM';
     hours = hours % 12;
     let output = `${hours}:${zeroPad(date.getMinutes(), 2)}:${zeroPad(date.getSeconds(), 2)} ${period}`;
 
@@ -41,4 +45,26 @@ const clock = (timestamp) => {
     window.requestAnimationFrame(clock);
 }
 
+const refresh = async () => {
+    var response = await fetch('http://tgwth.rh.rit.edu:3000/feed.json');
+    var newItems = await response.json();
+
+    var currentDate = Date.now();
+    items = newItems.filter((item) => { return item.expirationDate > currentDate });
+
+    var itemsString = '';
+    for(item of items) {
+        itemsString += `<div class="item content"><h2>${item.title}</h2></div>`
+    }
+
+    document.getElementById('items').innerHTML = itemsString;
+
+    document.getElementById('details').innerHTML = `<h2>${items[currentItem].title}</h2><p>${items[currentItem].description}</p>`;
+    currentItem = (currentItem + 1) % items.length;
+};
+
+setInterval(refresh, 15000);
+
 window.onload = init;
+
+refresh();
